f71e2a1be99a8a643e1da9201e57ddd26ee84d41
Oleksandr Kostiuchenko <aleksandrkostucenko11@gmail.com>
2025-04-25 23:07:37 +0200

    fix-loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -5,12 +5,7 @@ import { InfinitySpin } from "react-loader-spinner";
 const Loader = () => {
   return (
     <div className={style.backdrop}>
-      <InfinitySpin
-        visible={true}
-        width="200"
-        color="#4fa94d"
-        ariaLabel="infinity-spin-loading"
-      />
+      <InfinitySpin width="200" color="#4fa94d" />
     </div>
   );
 };
